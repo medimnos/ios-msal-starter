400dbcacb32c067adb95ff283914347d39f59357
Uğur Uğurlu <medimnos@hotmail.com>
2020-04-07 23:26:15 +0300

    Initial Commit

---
+++ iOS-MSAL-Starter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-MSAL-Starter
+//
+//  Created by Uğur Uğurlu on 7.04.2020.
+//  Copyright © 2020 Ugur Ugurlu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
